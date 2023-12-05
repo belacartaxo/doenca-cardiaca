09b9ce88fc7f22aabaeb5b9e8e0c1ba200e49b02
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-12-05 00:44:56 +0000

    Data exploration

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import numpy as np
 import pandas as pd
+
+dados = pd.read_csv('./heart.csv', sep=',', encoding='iso-8859-1') 
+
+'''
+EXPLORAÇÃO DE DADOS
+    dados.head() - mostra as primeiras linhas
+    dados.head(x) - mostra as x primeiras linhas
+    dados.tail() =mostra as últimas linhas
+    dados.shape - mostra a quantidade de linhas e colunas
+'''
